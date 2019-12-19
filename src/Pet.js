45095512b9c12befb4b787cf1f7f1c987c44421d
Jean <rjeangonzalo@gmail.com>
2019-12-19 08:18:37 -0500

    [JSX] Converting to JSC

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
+    </div>
+
+    /* React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
     React.createElement("h2", {}, breed)
-  ]);
+  ]);  */
+  );
 };
 
 export default Pet;
