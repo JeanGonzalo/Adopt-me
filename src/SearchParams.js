895151883eafe5357b895430f22567c99256bbe4
Jean <rjeangonzalo@gmail.com>
2019-12-20 16:03:02 -0500

    [Hooks][useState] Creating && Setting State

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchParams = () => {
-    const location = "Seattle. WA";
+    const [location, setLocation] = useState("Seattle. WA");
 
     return (
         <div className="search-params">
+            <h1>{location}</h1>
             <form>
                 <label htmlFor="location">
                     location
                     <input id="location" value={location}
-                        placeholder="Location" />
+                        placeholder="Location"
+                        onChange={e => setLocation(e.target.value)} />
                 </label>
                 <button>Submit</button>
             </form>
