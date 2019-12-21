b21468b192a0b0b5f4e786e5c81a121e23af9082
Jean <rjeangonzalo@gmail.com>
2019-12-21 15:59:00 -0500

    [Hooks] Breed Dropdown

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -3,6 +3,9 @@ import { ANIMALS } from '@frontendmasters/pet';
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle. WA");
     const [animal, setAnimal] = useState("dog");
+    const [breed, setBreed] = useState("");
+    const [breeds, setBreeds] = useState([]);
+
     return (
         <div className="search-params">
             <h1>{location}</h1>
@@ -25,6 +28,23 @@ const SearchParams = () => {
                         {ANIMALS.map(animal => (
                             <option key={animal} value={animal}>
                                 {animal}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    breed
+                    <select
+                        id="breed"
+                        value={breed}
+                        onChange={e => setBreed(e.target.value)}
+                        onBlur={e => setBreed(e.target.value)}
+                        disabled={!breeds.length}
+                    >
+                        <option>All</option>
+                        {breeds.map(breedString => (
+                            <option key={breedString} value={breedString}>
+                                {breedString}
                             </option>
                         ))}
                     </select>
